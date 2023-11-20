592c0c3508e40f61fe264f08d1e60750e193868f
Jens <j.raunstrup@gmail.com>
2023-11-20 09:31:31 +0100

    add mixed-messages

--- CodeCademy/full-stack-engineer/mixed-messages/main.js
+++ CodeCademy/full-stack-engineer/mixed-messages/main.js
@@ -14,5 +14,31 @@
 //games that has a rating of at least 8.5. so its a quality rnadom game generator.
 
 //3.
-console.log("Random videogame recommendations");
+let arrayWithGames = [
+    ["Name: Elden Ring", "Genre: Action/RPG", "Platform: PS5/Xbox X/PS4/Xbox one/PC", "Metacritic score: 96"],
+    ["Name: The Witcher 3", "Genre: Action/RPG", "Platform: All platforms", "Metacritic score: 94"],
+    ["Name: Persona 5 Royal", "Genre: JRPG", "Platform: All platforms", "Metacritic score: 91"],
+    ["Name: GTA V", "Genre: Open-World-Action", "Platform: PC/Playstation/Xbox", "Metacritic score: 97"],
+    ["Name: Super Mario Odyssey", "Genre: 3D Platformer", "Platform: Nintendo switch", "Metacritic score: 97"],
+    ["Name: The Legend Of Zelda Breath Of The Wild", "Genre: Open-World-Action", "Platform: Nintendo Switch", "Metacritic score: 97"],
+    ["Name: Metal Gear Solid V: The Phantom Pain", "Genre: Stealh/Open-World", "Platform: Playstation/Xbox/PC", "Metacritic score: 93"],
+    ["Name: Hollow Knight", "Genre: Metroidvania", "Platform: All platforms", "Metacritic score: 90"],
+    ["Name: Resident Evil 4 remake", "Genre: Survivalhorror", "Platform: Playstation/Xbox/PC", "Metacritic score: 93"],
+    ["Name: God of War", "Genre: Action-adventure", "Platform: PS4", "Metacritic score: 94"]
+];
 
+const videoGameRecommender = (array) => {
+    console.log("");
+    console.log("Random Videogame Recommendation:");
+
+    let randomNumber = Math.floor(Math.random() * array.length);
+    let randomGame = array[randomNumber];
+
+    console.log("");
+    for (let i = 0; i < array[randomNumber].length; i++) {
+        console.log(randomGame[i]);
+    }
+    console.log("");
+}
+
+videoGameRecommender(arrayWithGames);
