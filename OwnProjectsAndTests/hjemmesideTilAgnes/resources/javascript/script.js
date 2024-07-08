58df7eed36dd4df9a42d2abe45a60ed8ddc8a472
Jens <j.raunstrup@gmail.com>
2024-07-08 13:53:18 +0200

    add first draft to AMRC website

---
+++ OwnProjectsAndTests/hjemmesideTilAgnes/resources/javascript/script.js
@@ -0,0 +1,50 @@
+let navn = "AMRC";
+
+let elementNumber = -1;
+
+
+
+const testFunction1 = () => {
+    let test2 = document.getElementById("testId1");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+const testFunction2 = () => {
+    let test2 = document.getElementById("testId2");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+const testFunction3 = () => {
+    let test2 = document.getElementById("testId3");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+const testFunction4 = () => {
+    let test2 = document.getElementById("testId4");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+const testFunction5 = () => {
+    let test2 = document.getElementById("testId5");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
