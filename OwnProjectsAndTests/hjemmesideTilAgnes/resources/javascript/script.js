edb9af456d11090fe6fc01435e9f97f66f1591b1
Jens <j.raunstrup@gmail.com>
2024-07-12 11:52:00 +0200

    add corrections to dropdown v2

--- OwnProjectsAndTests/hjemmesideTilAgnes/resources/javascript/script.js
+++ OwnProjectsAndTests/hjemmesideTilAgnes/resources/javascript/script.js
@@ -3,21 +3,12 @@ let navn = "AMRC";
 let elementNumber = -1;
 
 
-
 const testFunction1 = () => {
     let test2 = document.getElementById("kontaktSpanBox");
-    let test3 = document.getElementById("dropDown1");
-    let test4 = document.getElementById("dropDownUp1");
     if (test2.style.display === "flex") {
         test2.style.display = "none"
-        test3.style.display = "flex";
-        test4.style.display = "none"
     } else {
         test2.style.display = "flex";
-        test3.style.display = "none";
-        test4.style.display = "flex";
-        test4.style.paddingLeft = "16px";
-        test4.style.paddingRight = "0px";
     }
 }
 
