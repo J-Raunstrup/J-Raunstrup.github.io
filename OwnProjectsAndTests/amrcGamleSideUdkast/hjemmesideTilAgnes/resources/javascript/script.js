7f0489e16da13096f857a1c579ea5227dab84a3f
Jens <j.raunstrup@gmail.com>
2024-08-04 18:25:28 +0200

    try to make a new site for a

---
+++ OwnProjectsAndTests/amrcGamleSideUdkast/hjemmesideTilAgnes/resources/javascript/script.js
@@ -0,0 +1,51 @@
+let navn = "AMRC";
+
+let elementNumber = -1;
+
+
+const testFunction1 = () => {
+    let test2 = document.getElementById("kontaktSpanBox");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+const testFunction2 = () => {
+    let test2 = document.getElementById("showreelBoxIndhold");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+const testFunction3 = () => {
+    let test2 = document.getElementById("nytFraMigBoxIndhold");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+const testFunction4 = () => {
+    let test2 = document.getElementById("portfolieBoksIndhold");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
+
+/* add the following back again after testing. */
+
+const testFunction5 = () => {
+    let test2 = document.getElementById("fotosBoksIndhold");
+    if (test2.style.display === "flex") {
+        test2.style.display = "none"
+    } else {
+        test2.style.display = "flex";
+    }
+}
